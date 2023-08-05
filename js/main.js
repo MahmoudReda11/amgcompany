370116fbef840d6a19d8f6b8c61764730b2c24d5
MahmoudReda11 <115508120+MahmoudReda11@users.noreply.github.com>
2023-08-05 18:27:17 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,51 @@
+// swiper js 
+
+var swiper = new Swiper(".mySwiper", {
+    grabCursor: true,
+    spaceBetween: 30,
+    slidesPerView: "4",
+    loop: true,
+    autoplay:{
+      delay:3000,
+    },
+    breakpoints:{
+1200:{
+slidesPerView : 4,
+},
+990 : {
+slidesPerView : 4,
+},
+500 :{
+slidesPerView : 3,
+},
+0:{
+  slidesPerView : 2
+}
+
+}
+  });
+
+
+  // change background
+
+  x = 0;
+  function changeBg(){
+      const images = [
+          'linear-gradient(#253169b4,#253169b4),url("./image/slide1.jpg")',
+          'linear-gradient(#253169b4,#253169b4),url("./image/slider2.jpg")',
+          'linear-gradient(#253169b4,#253169b4),url("./image/slider3.jpg")',
+          'linear-gradient(#253169b4,#253169b4),url("./image/slider4.jpg")'
+      ]
+     
+      const bg = x++;
+      if( x >= 4){
+          x = 0;
+      }
+      
+      const sectionbg = document.querySelector(".swiper-sec")
+      sectionbg.style.backgroundImage = images[bg];
+     
+      console.log(x)        
+  }
+  setInterval(changeBg, 3000);
+  
